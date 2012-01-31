175e3fb67987e90d4333effea0457f57913f4160
Stephen Cleary <StephenCleary@users.noreply.github.com>
2012-01-30 23:44:33 -0500

    Added VS template for async unit tests.

--- Source/Scripts/uninstall.ps1
+++ Source/Scripts/uninstall.ps1
@@ -6,6 +6,7 @@ $machineConfigReg = "hklm:\" + $project.DTE.RegistryRoot + "_Config"
 $userConfigReg = "hkcu:\" + $project.DTE.RegistryRoot + "_Config"
 $installDir = (Get-ItemProperty -Path ($machineReg + "\Setup\VS")).ProductDir + "Common7\IDE\PrivateAssemblies"
 $existingDllPath = $installDir + "\Nito.AsyncEx.UnitTests.dll"
+$targetTemplatePath = (Get-ItemProperty -Path $userReg).UserItemTemplatesLocation + "\Visual C#\AsyncUnitTest.zip"
 
 function Uninstall($path)
 {
@@ -21,5 +22,6 @@ Uninstall ($userReg + $regSubkey)
 Uninstall ($machineConfigReg + $regSubkey)
 Uninstall ($userConfigReg + $regSubkey)
 Uninstall $existingDllPath
+Uninstall $targetTemplatePath
 
 "Uninstall completed."
