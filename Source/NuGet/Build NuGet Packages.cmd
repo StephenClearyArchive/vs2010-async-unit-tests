75e9c82014215e590dc8388b76b5d7a8ae05e689
Stephen Cleary <Stephen Cleary@localhost>
2012-02-01 10:24:31 -0500

    Restructured the current solution to include an "MSTest" designation at several points. Hopefully in the near future we can support NUnit and xUnit as well. The NuGet package has also been renamed (AsyncUnitTests-MSTest).

--- Source/NuGet/Build NuGet Packages.cmd
+++
@@ -1,5 +0,0 @@
-@echo off
-if not exist ..\..\Binaries mkdir ..\..\Binaries
-if not exist ..\..\Binaries\NuGet mkdir ..\..\Binaries\NuGet
-..\..\Util\nuget.exe pack -sym AsyncUnitTests.nuspec -o ..\..\Binaries\NuGet
-pause
